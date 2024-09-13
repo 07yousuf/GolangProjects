e2697eed435e04e1e7fddd95c63252915ef98ab3
yousuf <ysyousuf500@gmail.com>
2024-09-14 01:23:25 +0600

    complete without DB logic

--- ProductStocks/middleware/handlers.go
+++ ProductStocks/middleware/handlers.go
@@ -38,18 +38,92 @@ func createConnection() *sql.DB{
 }
 
   GetStock(w http.ResponseWriter, r http.Request){
-
+    params := mux.Vars(r)
+    id,err := strconv.Atoi(params["id"])
+    if err != nil{
+      log.Fatalf("unable to convert the string into int. %v",err)
+    }
+    stock, err := getStock(int64(id))
+    if err != nil {
+      log.Fatalf("unable to get stock %v", err)
+    }
+    json.NewEncoder(w).Encode(stock)
   }
-  GetStocks(w http.ResponseWriter, r http.Request){
 
+  GetAllStock(w http.ResponseWriter, r http.Request){
+    stocks, err := getAllStocks()
+    if err != nil {
+      log.Fatalf("unable to get all stock %v",err)
+    }
+    json.NewEncoder(w).Encode(stocks)
   }
-  CreateStocks(w http.ResponseWriter, r http.Request){
 
+  CreateStock(w http.ResponseWriter, r http.Request){
+    var stock models.Stock
+    err:= json.NewDecoder(r.Body).Decode(&stock)
+    if err != nil{
+      log.Fatal("Unable to decode the request body. %v",err)
+    }
+    insertID := response{
+      ID: insertID,
+      Message: "Stock created Succesfully",
+    }
+    json.NewEncoder(w).Encode(res)
   }
+
   UpdateStocks(w http.ResponseWriter, r http.Request){
-
+    params := mux.Vars(r)
+    id, err := strconv.Atoi(params["id"])
+    if err != nil {
+      log.Fatalf("unable to convert the string into int. %v", err)
+    }
+      var stock models.Stock
+      err = json.NewDecoder(r.Body).Decode(&stock)
+      
+      if err != nil{
+        log.Fatalf("unable to decode the request body. %v",err)
+      }
+      updateRows := updateStock(int64(id), stock)
+      msg := fmt.Sprint("Stock updated Succesfully, total row/record affected %v",updatedRows)
+      res := response{
+        ID: int64(id),
+        Message: msg,
+      }
+      json.NewEncoder(w).Encode(res)
   }
   DeleteStocks(w http.ResponseWriter, r http.Request){
+    params := mux.Vars(r)
+    id, err := strconv.Atoi(params["id"])
 
+    if err != nil{
+      log.Fatalf("unable to convert the string into int. %v",err)
+    }
+    deletedRows := deleteStock(int64(id))
+    msg := fmt.Sprintf("Stocks updated Succesfully. Total rows/record affected %v",deletedRows)
+    res := response{
+      ID: int64(id),
+      Message: msg,
+    }
+    json.NewEncoder(w).Encode(res)
+  }
+
+  //Handler functions---
+
+  func GetAllStocks()([]models.Stocks, error)
+  db := createConnection()
+  defer db.close()
+  var stocks []models.stocks
+  sqlStatement := `SELECTS * FROM stocks`
+  rows, err := db.Query(sqlStatement)
+  if err != nil {
+    log.Fatalf("unable to excute the query. %v", err)
+  }
+  defer rows.Close()
+  for rows.Next(){
+    var stock models.Stock 
+     err = rows.Scan(&stock.StockID,&stock.Name,&stock.Price,&stock.Company)
+     if err != nil {
+      
+     }
   }
 }
